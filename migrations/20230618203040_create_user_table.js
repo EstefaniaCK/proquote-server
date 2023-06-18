af19b94610d009d3e5ba512f43fe214b282b42fd
Estefck <estef.komu@gmail.com>
2023-06-18 16:51:07 -0400

    Created migration for users table

---
+++ migrations/20230618203040_create_user_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', function (table) {
+        table.increments('id').primary();
+        table.string('company_name').notNullable;
+        table.string('email').notNullable;
+        table.string('password').notNullable;
+    });
+};
+
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('users');
+};
