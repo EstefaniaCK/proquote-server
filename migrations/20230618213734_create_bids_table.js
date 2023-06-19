d0b6ace1c026ba14b0462f70d79c021cdbd09312
Estefck <estef.komu@gmail.com>
2023-06-18 21:16:32 -0400

    Created new migration file for the bids table to fix error before seeding

--- migrations/20230618213734_create_bids_table.js
+++ migrations/20230618213734_create_bids_table.js
@@ -4,15 +4,17 @@ exports.up = function (knex) {
         table.increments('id').primary();
         table.integer('item_id').unsigned().notNullable();
         table.integer('user_id').unsigned().notNullable();
+        table.integer('project_id').unsigned().notNullable();
         table.decimal('unit_price', 10, 2).notNullable();
+
 
         table.foreign('item_id').references('items.id');
         table.foreign('user_id').references('users.id');
+        table.foreign('project_id').references('projects.id');
     });
 };
 
 
 exports.down = function (knex) {
     return knex.schema.dropTable('bids');
-
 };
