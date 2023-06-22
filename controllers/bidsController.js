73b567518442241d097ce02152a56ec66fe645a8
Estefck <estef.komu@gmail.com>
2023-06-22 17:28:17 -0400

    Functionality for posting the new bids

--- controllers/bidsController.js
+++ controllers/bidsController.js
@@ -4,14 +4,24 @@ const knex = require('knex')(require('../knexfile'));
 const addBids = async (req, res) => {
     try {
         console.log("LOOK AT MEE", req.body);
-        await knex('users-bids').insert(
-          (req.body) 
+        const [bidId] = await knex('bids').insert(
+            { project_id: req.body.project_id, user_id: req.body.user_id }
         );
-    res.status(200).json({ message: 'Bids added successfully' });
-} catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Failed to add bids' });
-}
+
+        await knex('users-bids').insert(
+            (req.body.pricingList.map(
+                (price) => {
+                    return {
+                        ...price, bid_id: bidId
+                    }
+                }
+            ))
+        );
+        res.status(200).json({ message: 'Bids added successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to add bids' });
+    }
 };
 
 module.exports = {
