8c400fe954c32daa396427aa8a0478df9a811e38
Estefck <estef.komu@gmail.com>
2023-06-26 11:39:30 -0400

    Seed files have been adjusted

--- seed-data/users-bids.js
+++ seed-data/users-bids.js
@@ -89,4 +89,124 @@ module.exports = [
         item_id: 10,
         unit_price: 5
     },
+    {
+        id: 16,
+        bid_id: 4,
+        item_id: 6,
+        unit_price: 6
+    },
+    {
+        id: 17,
+        bid_id: 4,
+        item_id: 7,
+        unit_price: 7
+    },
+    {
+        id: 18,
+        bid_id: 4,
+        item_id: 8,
+        unit_price: 7
+    },
+    {
+        id: 19,
+        bid_id: 4,
+        item_id: 9,
+        unit_price: 7
+    },
+    {
+        id: 20,
+        bid_id: 4,
+        item_id: 10,
+        unit_price: 7
+    },
+    {
+        id: 21,
+        bid_id: 4,
+        item_id: 11,
+        unit_price: 3
+    },
+    {
+        id: 22,
+        bid_id: 4,
+        item_id: 12,
+        unit_price: 6
+    },
+    {
+        id: 23,
+        bid_id: 4,
+        item_id: 13,
+        unit_price: 9
+    },
+    {
+        id: 24,
+        bid_id: 4,
+        item_id: 14,
+        unit_price: 3
+    },
+    {
+        id: 25,
+        bid_id: 4,
+        item_id: 15,
+        unit_price: 1
+    },
+    {
+        id: 26,
+        bid_id: 5,
+        item_id: 16,
+        unit_price: 3
+    },
+    {
+        id: 27,
+        bid_id: 5,
+        item_id: 17,
+        unit_price: 6
+    },
+    {
+        id: 28,
+        bid_id: 5,
+        item_id: 18,
+        unit_price: 9
+    },
+    {
+        id: 29,
+        bid_id: 5,
+        item_id: 19,
+        unit_price: 3
+    },
+    {
+        id: 30,
+        bid_id: 5,
+        item_id: 20,
+        unit_price: 1
+    },
+    {
+        id: 31,
+        bid_id: 5,
+        item_id: 21,
+        unit_price: 3
+    },
+    {
+        id: 32,
+        bid_id: 5,
+        item_id: 22,
+        unit_price: 6
+    },
+    {
+        id: 33,
+        bid_id: 5,
+        item_id: 23,
+        unit_price: 9
+    },
+    {
+        id: 34,
+        bid_id: 5,
+        item_id: 24,
+        unit_price: 3
+    },
+    {
+        id: 35,
+        bid_id: 5,
+        item_id: 25,
+        unit_price: 1
+    },
 ]
